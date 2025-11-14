8d7bb0d57a6e0e8550ea2508ec234d1c846145ab
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-11-14 10:34:59 -0500

    Create ServiceController.php

---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ServiceController extends Controller
+{
+    public function index()
+    {
+        return "Service Page Loaded Successfully — Module 2";
+    }
+}
