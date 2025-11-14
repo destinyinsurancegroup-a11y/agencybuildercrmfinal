bd08f0cf96f84f6e81ed50d93e5baff7aa3f255b
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-11-14 10:34:17 -0500

    Create MyNumbersController.php

---
+++ app/Http/Controllers/MyNumbersController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class MyNumbersController extends Controller
+{
+    public function index()
+    {
+        return "My Numbers Loaded Successfully — Module 2";
+    }
+}
