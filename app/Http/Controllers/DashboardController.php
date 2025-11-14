8bec2464571a2263f2e703e70eefa1cec62ae8da
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-11-14 10:31:41 -0500

    Create DashboardController.php

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        return "Dashboard Loaded Successfully — Module 2";
+    }
+}
