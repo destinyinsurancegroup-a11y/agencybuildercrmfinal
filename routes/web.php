d8f96d464750090b9d04467fde6f0feaf530b2a2
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-11-15 14:49:31 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
+// TEST ROUTE TO CONFIRM ROUTES WORK
 Route::get('/test', function () {
     return 'ROUTES ARE WORKING';
 });
-
-// -----------------------
-// existing routes below
-// -----------------------
-
-Route::get('/', function () {
-    return view('dashboard');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-});
-
-Route::get('/contacts', function () {
-    return view('contacts.index');
-});
-
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
 
 // Dashboard
 Route::get('/', function () {
