c6b622fd53aa5786d603f9d0a8ef7ef66852b549
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-11-15 15:00:01 -0500

    Create index.blade.php

---
+++ resources/views/calendar/index.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <div style="padding: 40px;">
+        <h1 style="font-size: 28px; font-weight: 700;">Calendar</h1>
+        <p style="margin-top: 10px; font-size: 16px; color: #666;">
+            This is the Calendar page placeholder.
+        </p>
+    </div>
+</x-app-layout>
