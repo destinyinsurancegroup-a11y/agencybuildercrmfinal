6f3e5f604614d8aa290ddb76239511347e94d160
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-11-15 15:47:17 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Http\Request;
+use App\Models\Event;
 
-// TEST ROUTE
+/*
+|--------------------------------------------------------------------------
+| TEST ROUTE
+|--------------------------------------------------------------------------
+*/
 Route::get('/test', function () {
     return 'ROUTES ARE WORKING';
 });
 
-// Dashboard
+/*
+|--------------------------------------------------------------------------
+| DASHBOARD
+|--------------------------------------------------------------------------
+*/
 Route::get('/', function () {
     return view('dashboard');
 });
@@ -17,17 +27,54 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 });
 
-// Contacts
+/*
+|--------------------------------------------------------------------------
+| CONTACTS
+|--------------------------------------------------------------------------
+*/
 Route::get('/contacts', function () {
     return view('contacts.index');
 });
 
-// Calendar
+/*
+|--------------------------------------------------------------------------
+| CALENDAR PAGE
+|--------------------------------------------------------------------------
+*/
 Route::get('/calendar', function () {
     return view('calendar.index');
 });
 
-// TEMPORARY CLEAR CACHE
+/*
+|--------------------------------------------------------------------------
+| CALENDAR API ROUTES
+| Load events + Save events
+|--------------------------------------------------------------------------
+*/
+
+// Return all events for the calendar
+Route::get('/calendar/events', function () {
+    return Event::all();
+});
+
+// Add new event created in the calendar
+Route::post('/calendar/events', function (Request $request) {
+    return Event::create([
+        'title' => $request->title,
+        'start' => $request->start,
+        'end'   => $request->end,
+        'color' => $request->color,
+        // tenant and user will be handled later
+        'tenant_id' => 1,
+        'created_by' => null,
+    ]);
+});
+
+/*
+|--------------------------------------------------------------------------
+| TEMPORARY CLEAR CACHE
+|--------------------------------------------------------------------------
+*/
 Route::get('/clear-cache', function () {
     Artisan::call('route:clear');
     Artisan::call('config:clear');
