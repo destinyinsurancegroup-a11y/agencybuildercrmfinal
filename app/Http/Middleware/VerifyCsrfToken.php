5a114cae69b5037ecbcc3462d7d35b182b189e88
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-11-15 20:16:52 -0500

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -9,9 +9,11 @@ class VerifyCsrfToken extends Middleware
     /**
      * The URIs that should be excluded from CSRF verification.
      *
-     * @var array<int, string>
+     * For now, we exclude the calendar events endpoint so the JS
+     * fetch() call can POST without being blocked by a 419 error.
+     * Later, once auth is wired up, we can lock this back down.
      */
     protected $except = [
-        //
+        'calendar/events',
     ];
 }
