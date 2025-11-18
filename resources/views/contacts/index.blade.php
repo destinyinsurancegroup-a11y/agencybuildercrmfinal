66efa26ac9dba083757bb72e4604db10b0e60326
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-11-17 21:34:53 -0500

    Update index.blade.php

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -1,1 +1,8 @@
-<h1>Contacts Working</h1>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container py-5">
+        <h1>Contacts Page Working</h1>
+        <p>This confirms the route, controller, and view are all correct.</p>
+    </div>
+@endsection
