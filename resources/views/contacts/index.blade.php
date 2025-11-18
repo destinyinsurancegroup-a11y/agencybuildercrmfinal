b1d60dc729b590d8181da2f0b70be59744ffaa98
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-11-17 21:22:49 -0500

    Update index.blade.php

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -1,11 +1,1 @@
-<x-app-layout>
-    <div class="h-full flex gap-6">
-        <div class="w-[420px]">
-            <livewire:contacts.contacts-list />
-        </div>
-
-        <div class="flex-1">
-            <livewire:contacts.contact-details />
-        </div>
-    </div>
-</x-app-layout>
+<h1>Contacts Working</h1>
