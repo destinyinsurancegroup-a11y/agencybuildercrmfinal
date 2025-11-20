ad2fae7ba183993e69c77a3faaef6249041151ed
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-11-20 11:16:44 -0500

    Update details.blade.php

--- resources/views/leads/partials/details.blade.php
+++ resources/views/leads/partials/details.blade.php
@@ -7,7 +7,35 @@
 
     @if ($contact)
 
-        {{-- REUSE THE FULL CONTACT DETAILS PARTIAL --}}
+        {{-- HEADER WITH LEAD NAME --}}
+        <div class="card-header bg-white border-0 pt-4 px-4 pb-2">
+            <h2 class="fw-bold m-0">{{ $contact->first_name }} {{ $contact->last_name }}</h2>
+        </div>
+
+        {{-- DISPOSITION BUTTONS UNDER NAME --}}
+        <div class="px-4 pb-3 d-flex gap-2">
+
+            <button type="button"
+                class="btn btn-success btn-sm fw-bold"
+                onclick="alert('Sold action coming soon…');">
+                Sold
+            </button>
+
+            <button type="button"
+                class="btn btn-warning btn-sm fw-bold"
+                onclick="alert('Follow Up action coming soon…');">
+                Follow Up
+            </button>
+
+            <button type="button"
+                class="btn btn-danger btn-sm fw-bold"
+                onclick="alert('Not Interested action coming soon…');">
+                Not Interested
+            </button>
+
+        </div>
+
+        {{-- FULL CONTACT DETAILS CARD BELOW BUTTONS --}}
         @include('contacts.partials.details', ['contact' => $contact])
 
     @else
