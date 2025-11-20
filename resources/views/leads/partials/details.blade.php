2a3d8cae70ae68db5b4d4cec5b8d1332b5f15bd9
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-11-20 11:38:51 -0500

    Update details.blade.php

--- resources/views/leads/partials/details.blade.php
+++ resources/views/leads/partials/details.blade.php
@@ -1,19 +1,18 @@
 <div class="card h-100 shadow-sm border-0">
 
     @php
-        // Normalize variable naming
         $contact = $contact ?? $lead ?? null;
     @endphp
 
     @if ($contact)
 
-        {{-- HEADER WITH LEAD NAME --}}
+        {{-- LARGE NAME HEADING (single, no duplicate) --}}
         <div class="card-header bg-white border-0 pt-4 px-4 pb-2">
             <h2 class="fw-bold m-0">{{ $contact->first_name }} {{ $contact->last_name }}</h2>
         </div>
 
-        {{-- DISPOSITION BUTTONS --}}
-        <div class="px-4 pt-2 pb-1 d-flex gap-2">
+        {{-- DISPOSITION BUTTONS BELOW NAME --}}
+        <div class="px-4 pt-2 pb-3 d-flex gap-2">
 
             <button type="button"
                 class="btn btn-success btn-sm fw-bold"
@@ -35,16 +34,7 @@
 
         </div>
 
-        {{-- UPLOAD LEADS BUTTON --}}
-        <div class="px-4 pb-3 pt-1">
-            <button class="btn-gold btn-sm fw-bold"
-                    data-bs-toggle="modal"
-                    data-bs-target="#uploadLeadModal">
-                Upload Leads File
-            </button>
-        </div>
-
-        {{-- CONTACT DETAILS CARD --}}
+        {{-- FULL CONTACT CARD BELOW --}}
         @include('contacts.partials.details', ['contact' => $contact])
 
     @else
@@ -53,43 +43,4 @@
         </div>
     @endif
 
-</div>
-
-
-{{-- ========================= --}}
-{{-- LEADS UPLOAD MODAL --}}
-{{-- ========================= --}}
-
-<div class="modal fade" id="uploadLeadModal" tabindex="-1">
-    <div class="modal-dialog">
-        <form 
-            action="{{ route('contacts.import') }}" 
-            method="POST" 
-            enctype="multipart/form-data"
-            class="modal-content"
-        >
-            @csrf
-
-            <div class="modal-header bg-black text-gold">
-                <h5 class="modal-title">Upload Leads File</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-            </div>
-
-            <div class="modal-body">
-                <label class="form-label">Choose CSV or Excel file</label>
-                <input 
-                    type="file"
-                    name="file"
-                    class="form-control"
-                    accept=".csv, .xlsx, .xls"
-                    required
-                >
-            </div>
-
-            <div class="modal-footer">
-                <button type="submit" class="btn-gold">Upload</button>
-            </div>
-
-        </form>
-    </div>
 </div>
