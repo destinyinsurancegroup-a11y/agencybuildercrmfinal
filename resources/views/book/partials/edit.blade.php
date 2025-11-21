57b746cd6e7bc366b4a7e7760dbe2a43a9b8f0a5
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-11-21 07:19:38 -0500

    Update edit.blade.php

--- resources/views/book/partials/edit.blade.php
+++ resources/views/book/partials/edit.blade.php
@@ -29,83 +29,34 @@
                 </div>
             </div>
 
-            <div class="row mb-4">
-                <div class="col-md-6">
-                    <label class="form-label fw-semibold">Email</label>
-                    <input type="email"
-                           name="email"
-                           class="form-control"
-                           value="{{ old('email', $client->email) }}">
-                </div>
-
-                <div class="col-md-6">
-                    <label class="form-label fw-semibold">Phone</label>
-                    <input type="text"
-                           name="phone"
-                           class="form-control"
-                           value="{{ old('phone', $client->phone) }}">
-                </div>
-            </div>
-
-            <div class="row mb-4">
-                <div class="col-md-6">
+            <div class="row mb-3">
+                <div class="col-md-4">
                     <label class="form-label fw-semibold">Date of Birth</label>
                     <input type="date"
                            name="date_of_birth"
                            class="form-control"
                            value="{{ old('date_of_birth', $client->date_of_birth) }}">
                 </div>
-            </div>
 
-            <hr>
-
-            <!-- POLICY INFORMATION -->
-            <h5 class="mb-3 text-gold fw-bold">Policy Information</h5>
-
-            <div class="row mb-3">
-                <div class="col-md-6">
-                    <label class="form-label fw-semibold">Policy Type</label>
-                    <input type="text"
-                           name="policy_type"
-                           class="form-control"
-                           value="{{ old('policy_type', $client->policy_type) }}">
-                </div>
-
-                <div class="col-md-3">
-                    <label class="form-label fw-semibold">Face Amount</label>
-                    <input type="number"
-                           step="0.01"
-                           name="face_amount"
-                           class="form-control"
-                           value="{{ old('face_amount', $client->face_amount) }}">
-                </div>
-
-                <div class="col-md-3">
-                    <label class="form-label fw-semibold">Premium Amount</label>
-                    <input type="number"
-                           step="0.01"
-                           name="premium_amount"
-                           class="form-control"
-                           value="{{ old('premium_amount', $client->premium_amount) }}">
-                </div>
-            </div>
-
-            <div class="row mb-4">
                 <div class="col-md-4">
-                    <label class="form-label fw-semibold">Premium Due Date</label>
+                    <label class="form-label fw-semibold">Age</label>
+                    <input type="number" class="form-control" disabled
+                           value="{{ $client->age }}">
+                </div>
+
+                <div class="col-md-4">
+                    <label class="form-label fw-semibold">Anniversary</label>
                     <input type="date"
-                           name="premium_due_date"
+                           name="anniversary"
                            class="form-control"
-                           value="{{ old('premium_due_date', $client->premium_due_date) }}">
+                           value="{{ old('anniversary', $client->anniversary) }}">
                 </div>
             </div>
-
-            <hr>
 
             <!-- ADDRESS -->
-            <h5 class="mb-3 text-gold fw-bold">Address</h5>
+            <h6 class="fw-bold mt-3 mb-2 text-gold">Address / Contact</h6>
 
-            <div class="mb-3">
+            <div class="mb-2">
                 <label class="form-label fw-semibold">Address Line 1</label>
                 <input type="text"
                        name="address_line1"
@@ -113,7 +64,7 @@
                        value="{{ old('address_line1', $client->address_line1) }}">
             </div>
 
-            <div class="mb-3">
+            <div class="mb-2">
                 <label class="form-label fw-semibold">Address Line 2</label>
                 <input type="text"
                        name="address_line2"
@@ -121,7 +72,7 @@
                        value="{{ old('address_line2', $client->address_line2) }}">
             </div>
 
-            <div class="row mb-4">
+            <div class="row mb-3">
                 <div class="col-md-4">
                     <label class="form-label fw-semibold">City</label>
                     <input type="text"
@@ -147,6 +98,182 @@
                 </div>
             </div>
 
+            <div class="row mb-4">
+                <div class="col-md-6">
+                    <label class="form-label fw-semibold">Phone</label>
+                    <input type="text"
+                           name="phone"
+                           class="form-control"
+                           value="{{ old('phone', $client->phone) }}">
+                </div>
+
+                <div class="col-md-6">
+                    <label class="form-label fw-semibold">Email</label>
+                    <input type="email"
+                           name="email"
+                           class="form-control"
+                           value="{{ old('email', $client->email) }}">
+                </div>
+            </div>
+
+            <hr>
+
+            <!-- POLICY INFORMATION -->
+            <h5 class="mb-3 text-gold fw-bold">Policy Information</h5>
+
+            <div class="row mb-3">
+                <div class="col-md-6">
+                    <label class="form-label fw-semibold">Carrier</label>
+                    <input type="text"
+                           name="carrier"
+                           class="form-control"
+                           value="{{ old('carrier', $client->carrier) }}">
+                </div>
+
+                <div class="col-md-6">
+                    <label class="form-label fw-semibold">Policy Type</label>
+                    <input type="text"
+                           name="policy_type"
+                           class="form-control"
+                           value="{{ old('policy_type', $client->policy_type) }}">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <div class="col-md-4">
+                    <label class="form-label fw-semibold">Face Amount</label>
+                    <input type="number"
+                           step="0.01"
+                           name="face_amount"
+                           class="form-control"
+                           value="{{ old('face_amount', $client->face_amount) }}">
+                </div>
+
+                <div class="col-md-4">
+                    <label class="form-label fw-semibold">Monthly Premium</label>
+                    <input type="number"
+                           step="0.01"
+                           name="premium_amount"
+                           class="form-control"
+                           value="{{ old('premium_amount', $client->premium_amount) }}">
+                </div>
+
+                <div class="col-md-4">
+                    <label class="form-label fw-semibold">Issue Date</label>
+                    <input type="date"
+                           name="policy_issue_date"
+                           class="form-control"
+                           value="{{ old('policy_issue_date', $client->policy_issue_date) }}">
+                </div>
+            </div>
+
+            <div class="mb-4">
+                <label class="form-label fw-semibold">Monthly Due Date (text)</label>
+                <input type="text"
+                       name="premium_due_text"
+                       class="form-control"
+                       placeholder="2nd Wednesday, 15th, etc"
+                       value="{{ old('premium_due_text', $client->premium_due_text) }}">
+            </div>
+
+            <hr>
+
+            <!-- BENEFICIARIES -->
+            <h5 class="mb-3 text-gold fw-bold">Beneficiaries</h5>
+
+            <div id="beneficiary-wrapper">
+
+                @foreach ($client->beneficiaries as $i => $b)
+                    <div class="beneficiary-row border rounded p-3 mb-3">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <label class="form-label fw-semibold">Name</label>
+                                <input type="text"
+                                       name="beneficiaries[{{ $i }}][name]"
+                                       class="form-control"
+                                       value="{{ $b->name }}">
+                            </div>
+
+                            <div class="col-md-3">
+                                <label class="form-label fw-semibold">Relationship</label>
+                                <input type="text"
+                                       name="beneficiaries[{{ $i }}][relationship]"
+                                       class="form-control"
+                                       value="{{ $b->relationship }}">
+                            </div>
+
+                            <div class="col-md-3">
+                                <label class="form-label fw-semibold">Phone</label>
+                                <input type="text"
+                                       name="beneficiaries[{{ $i }}][phone]"
+                                       class="form-control"
+                                       value="{{ $b->phone }}">
+                            </div>
+
+                            <div class="col-md-2">
+                                <label class="form-label fw-semibold">Contacted?</label>
+                                <select name="beneficiaries[{{ $i }}][contacted]" class="form-select">
+                                    <option value="0" {{ !$b->contacted ? 'selected' : '' }}>No</option>
+                                    <option value="1" {{ $b->contacted ? 'selected' : '' }}>Yes</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+
+            </div>
+
+            <button type="button" class="btn btn-sm btn-gold mb-3" id="add-beneficiary-btn">+ Add Beneficiary</button>
+
+            <hr>
+
+            <!-- EMERGENCY CONTACTS -->
+            <h5 class="mb-3 text-gold fw-bold">Emergency Contacts</h5>
+
+            <div id="emergency-wrapper">
+
+                @foreach ($client->emergencyContacts as $i => $e)
+                    <div class="emergency-row border rounded p-3 mb-3">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <label class="form-label fw-semibold">Name</label>
+                                <input type="text"
+                                       name="emergency_contacts[{{ $i }}][name]"
+                                       class="form-control"
+                                       value="{{ $e->name }}">
+                            </div>
+
+                            <div class="col-md-3">
+                                <label class="form-label fw-semibold">Relationship</label>
+                                <input type="text"
+                                       name="emergency_contacts[{{ $i }}][relationship]"
+                                       class="form-control"
+                                       value="{{ $e->relationship }}">
+                            </div>
+
+                            <div class="col-md-3">
+                                <label class="form-label fw-semibold">Phone</label>
+                                <input type="text"
+                                       name="emergency_contacts[{{ $i }}][phone]"
+                                       class="form-control"
+                                       value="{{ $e->phone }}">
+                            </div>
+
+                            <div class="col-md-2">
+                                <label class="form-label fw-semibold">Contacted?</label>
+                                <select name="emergency_contacts[{{ $i }}][contacted]" class="form-select">
+                                    <option value="0" {{ !$e->contacted ? 'selected' : '' }}>No</option>
+                                    <option value="1" {{ $e->contacted ? 'selected' : '' }}>Yes</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+
+            </div>
+
+            <button type="button" class="btn btn-sm btn-gold mb-3" id="add-emergency-btn">+ Add Emergency Contact</button>
+
             <hr>
 
             <!-- NOTES -->
@@ -163,9 +290,7 @@
 
             <!-- SUBMIT BUTTON -->
             <div class="text-end">
-                <button class="btn-gold btn-lg">
-                    Save Client
-                </button>
+                <button class="btn-gold btn-lg">Save Client</button>
             </div>
 
         </form>
@@ -173,3 +298,80 @@
     </div>
 
 </div>
+
+<script>
+let beneficiaryIndex = {{ count($client->beneficiaries) }};
+let emergencyIndex = {{ count($client->emergencyContacts) }};
+
+/* ADD BENEFICIARY */
+document.getElementById('add-beneficiary-btn').addEventListener('click', () => {
+    const wrapper = document.getElementById('beneficiary-wrapper');
+
+    wrapper.insertAdjacentHTML('beforeend', `
+        <div class="beneficiary-row border rounded p-3 mb-3">
+            <div class="row">
+                <div class="col-md-4">
+                    <label class="form-label fw-semibold">Name</label>
+                    <input type="text" name="beneficiaries[${beneficiaryIndex}][name]" class="form-control">
+                </div>
+
+                <div class="col-md-3">
+                    <label class="form-label fw-semibold">Relationship</label>
+                    <input type="text" name="beneficiaries[${beneficiaryIndex}][relationship]" class="form-control">
+                </div>
+
+                <div class="col-md-3">
+                    <label class="form-label fw-semibold">Phone</label>
+                    <input type="text" name="beneficiaries[${beneficiaryIndex}][phone]" class="form-control">
+                </div>
+
+                <div class="col-md-2">
+                    <label class="form-label fw-semibold">Contacted?</label>
+                    <select name="beneficiaries[${beneficiaryIndex}][contacted]" class="form-select">
+                        <option value="0">No</option>
+                        <option value="1">Yes</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    `);
+
+    beneficiaryIndex++;
+});
+
+/* ADD EMERGENCY CONTACT */
+document.getElementById('add-emergency-btn').addEventListener('click', () => {
+    const wrapper = document.getElementById('emergency-wrapper');
+
+    wrapper.insertAdjacentHTML('beforeend', `
+        <div class="emergency-row border rounded p-3 mb-3">
+            <div class="row">
+                <div class="col-md-4">
+                    <label class="form-label fw-semibold">Name</label>
+                    <input type="text" name="emergency_contacts[${emergencyIndex}][name]" class="form-control">
+                </div>
+
+                <div class="col-md-3">
+                    <label class="form-label fw-semibold">Relationship</label>
+                    <input type="text" name="emergency_contacts[${emergencyIndex}][relationship]" class="form-control">
+                </div>
+
+                <div class="col-md-3">
+                    <label class="form-label fw-semibold">Phone</label>
+                    <input type="text" name="emergency_contacts[${emergencyIndex}][phone]" class="form-control">
+                </div>
+
+                <div class="col-md-2">
+                    <label class="form-label fw-semibold">Contacted?</label>
+                    <select name="emergency_contacts[${emergencyIndex}][contacted]" class="form-select">
+                        <option value="0">No</option>
+                        <option value="1">Yes</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    `);
+
+    emergencyIndex++;
+});
+</script>
