484a89ff843d82d2201a038543311a73a0932d72
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-11-22 05:40:31 -0500

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Event;
+use App\Models\CrmEvent;
 use Illuminate\Support\Carbon;
 
 class DashboardController extends Controller
@@ -15,8 +15,8 @@ class DashboardController extends Controller
         // End Date = 7 days from now
         $endDate = Carbon::now()->addDays(7);
 
-        // Fetch events in the next 7 days
-        $events = Event::where('start', '>=', $startDate)
+        // Fetch events in the next 7 days from crm_events table
+        $events = CrmEvent::where('start', '>=', $startDate)
             ->where('start', '<=', $endDate)
             ->orderBy('start', 'asc')
             ->get();
