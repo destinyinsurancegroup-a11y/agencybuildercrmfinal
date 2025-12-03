1e466747a99afcee8fe63bb9073c4f1f04456af7
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-12-03 13:22:53 -0500

    Update ServiceEvent.php

--- app/Models/ServiceEvent.php
+++ app/Models/ServiceEvent.php
@@ -4,16 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Concerns\TenantScoped;
 
 class ServiceEvent extends Model
 {
-    use HasFactory;
+    use HasFactory, TenantScoped;
 
     protected $fillable = [
         'contact_id',
         'event_date',
         'event_type',
         'notes',
+        'agency_id',   // multi-tenant scoping field
     ];
 
     public function contact()
