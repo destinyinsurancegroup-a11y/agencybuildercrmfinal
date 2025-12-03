be4f5e1d5fe838e79c339e09a38d2ffd9fcc563b
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-12-03 15:01:38 -0500

    Update CrmEvent.php

--- app/Models/CrmEvent.php
+++ app/Models/CrmEvent.php
@@ -3,20 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Concerns\TenantScoped;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CrmEvent extends Model
 {
+    use HasFactory, TenantScoped;
+
     protected $table = 'crm_events';
 
     protected $fillable = [
         'title',
         'start',
         'end',
-        'color'
+        'color',
+        'agency_id',   // NEW: multi-tenant scoping field
     ];
 
     protected $casts = [
         'start' => 'datetime',
-        'end' => 'datetime',
+        'end'   => 'datetime',
     ];
 }
