21d6c6dfb47245922190edf143e8d2d9b399bc2a
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-12-03 12:15:54 -0500

    Create 2025_02_01_000011_backfill_contacts_agency_id.php

---
+++ database/migrations/2025_02_01_000011_backfill_contacts_agency_id.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        // Get the first (current) agency ID
+        $agencyId = DB::table('agencies')->value('id');
+
+        if ($agencyId) {
+            DB::table('contacts')
+                ->whereNull('agency_id')
+                ->update(['agency_id' => $agencyId]);
+        }
+    }
+
+    public function down(): void
+    {
+        // Rollback: clear agency_id on all contacts
+        DB::table('contacts')->update(['agency_id' => null]);
+    }
+};
