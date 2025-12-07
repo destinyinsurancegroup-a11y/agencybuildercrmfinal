533e1b348905284b61b2692d950da30f13b10217
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-12-06 21:48:14 -0500

    Update CrmEvent.php

--- app/Models/CrmEvent.php
+++ app/Models/CrmEvent.php
@@ -3,25 +3,48 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Concerns\TenantScoped;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Concerns\TenantScoped;
 
 class CrmEvent extends Model
 {
     use HasFactory, TenantScoped;
 
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
     protected $table = 'crm_events';
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
+        'agency_id',   // multi-tenant scoping field
         'title',
         'start',
         'end',
         'color',
-        'agency_id',   // NEW: multi-tenant scoping field
     ];
 
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
     protected $casts = [
         'start' => 'datetime',
         'end'   => 'datetime',
     ];
+
+    /**
+     * The agency (tenant) that owns this CRM event.
+     */
+    public function agency()
+    {
+        return $this->belongsTo(Agency::class);
+    }
 }
