b43db87c654d3f24892b89d2c35d595ccf6800b4
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-12-09 06:43:43 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,34 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Gideon controller
+use App\Http\Controllers\Gideon\GideonChatController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| These routes are loaded by the RouteServiceProvider and assigned to
+| the "api" middleware group.
 |
 */
 
+/*
+|--------------------------------------------------------------------------
+| Current logged-in user (requires auth)
+|--------------------------------------------------------------------------
+*/
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/*
+|--------------------------------------------------------------------------
+| GIDEON SPARRING PARTNER ENDPOINT
+|---------------------------------------------------------------------------
+| TEMPORARILY PUBLIC for testing (no auth required yet).
+| We will secure this later once UI integration begins.
+|--------------------------------------------------------------------------
+*/
+Route::post('/gideon/ask', [GideonChatController::class, 'ask']);
