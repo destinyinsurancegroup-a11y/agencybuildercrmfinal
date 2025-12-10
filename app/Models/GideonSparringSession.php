fc735d1f3e1a27e2372f2615ff6922299616562b
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-12-10 06:57:08 -0500

    Create GideonSparringSession model with relationships

---
+++ app/Models/GideonSparringSession.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class GideonSparringSession extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'agency_id',
+        'user_id',
+        'mode',
+        'persona_key',
+        'config',
+        'status',
+        'started_at',
+        'ended_at',
+    ];
+
+    protected $casts = [
+        'config'     => 'array',
+        'started_at' => 'datetime',
+        'ended_at'   => 'datetime',
+    ];
+
+    public function messages()
+    {
+        return $this->hasMany(GideonSparringMessage::class, 'session_id');
+    }
+
+    public function assessment()
+    {
+        return $this->hasOne(GideonSparringAssessment::class, 'session_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function agency()
+    {
+        return $this->belongsTo(Agency::class);
+    }
+}
