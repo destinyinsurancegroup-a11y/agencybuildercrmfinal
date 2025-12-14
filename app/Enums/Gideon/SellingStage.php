71c897535e4a3fa19db110da5c758e1d5a407ef9
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-12-14 07:12:35 -0500

    Create SellingStage.php

---
+++ app/Enums/Gideon/SellingStage.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Enums\Gideon;
+
+enum SellingStage: string
+{
+    case INTRO = 'intro';
+    case DISCOVERY = 'discovery';
+    case EDUCATION = 'education';
+    case QUALIFY = 'qualify';
+    case QUOTE = 'quote';
+    case CLOSE = 'close';
+
+    public static function ordered(): array
+    {
+        return [self::INTRO, self::DISCOVERY, self::EDUCATION, self::QUALIFY, self::QUOTE, self::CLOSE];
+    }
+
+    public function next(): ?self
+    {
+        $ordered = self::ordered();
+        $i = array_search($this, $ordered, true);
+        if ($i === false) return null;
+        return $ordered[$i + 1] ?? null;
+    }
+}
