40dda03b472b1299beda519e33d2bec8557d429c
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-12-14 07:10:01 -0500

    Create TrainingMode.php

---
+++ app/Enums/Gideon/TrainingMode.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums\Gideon;
+
+enum TrainingMode: string
+{
+    case STAGES = 'stages';
+    case DISCOVERY_START = 'discovery_start';
+    case FULL_PRESENTATION = 'full_presentation';
+}
