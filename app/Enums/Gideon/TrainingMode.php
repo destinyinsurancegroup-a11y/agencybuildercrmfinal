3113b9b79ed17fcc6c1617cc1317d30e875bb338
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-12-17 06:54:15 -0500

    Update TrainingMode.php

--- app/Enums/Gideon/TrainingMode.php
+++ app/Enums/Gideon/TrainingMode.php
@@ -4,7 +4,27 @@ namespace App\Enums\Gideon;
 
 enum TrainingMode: string
 {
-    case STAGES = 'stages';
-    case DISCOVERY_START = 'discovery_start';
+    case STAGES            = 'stages';
+    case DISCOVERY_START   = 'discovery_start';
     case FULL_PRESENTATION = 'full_presentation';
+
+    /**
+     * Safe normalization helper.
+     * Accepts:
+     * - TrainingMode enum
+     * - string (valid value)
+     * - null (defaults)
+     */
+    public static function normalize(self|string|null $mode, self $default = self::DISCOVERY_START): self
+    {
+        if ($mode instanceof self) {
+            return $mode;
+        }
+
+        if (is_string($mode)) {
+            return self::tryFrom($mode) ?? $default;
+        }
+
+        return $default;
+    }
 }
