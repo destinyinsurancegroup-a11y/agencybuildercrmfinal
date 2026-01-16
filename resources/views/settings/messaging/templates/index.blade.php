928dfd150a84324959da60b58f8c57ab96877f49
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2026-01-16 16:28:26 -0500

    Update index.blade.php

--- resources/views/settings/messaging/templates/index.blade.php
+++ resources/views/settings/messaging/templates/index.blade.php
@@ -6,12 +6,11 @@
 
 @section('settings_content')
 <div class="panel-card">
-    <div class="d-flex align-items-center justify-content-between mb-3">
-        <h2 class="mb-0" style="font-weight:900;">Message Templates</h2>
+    <div class="panel-card-body">
+        <h2 class="mb-2" style="font-weight:900;">Message Templates</h2>
+        <p class="text-muted mb-0">
+            Create reusable SMS and Email templates (with variables like <code>{{'{{first_name}}'}}</code>).
+        </p>
     </div>
-
-    <p class="text-muted mb-0">
-        Create reusable SMS and Email templates (with variables like <code>{{'{{first_name}}'}}</code>).
-    </p>
 </div>
 @endsection
