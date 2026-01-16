2a985d7d999458d2b921ed085aeab76b3efb85a8
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2026-01-16 12:45:55 -0500

    Create index.blade.php

---
+++ resources/views/settings/messaging/templates/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="p-6">
+    <h1 class="text-2xl font-semibold">Drip Campaigns</h1>
+    <p class="mt-2 text-sm text-gray-600">
+        Configure onboarding and milestone campaigns.
+    </p>
+</div>
+@endsection
